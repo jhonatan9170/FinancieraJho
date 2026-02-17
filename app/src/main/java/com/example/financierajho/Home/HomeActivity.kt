61870d82e98c57b649209d7788fd91a3895d0213
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2026-02-16 21:00:52 -0500

    sharedpreferences

---
+++ app/src/main/java/com/example/financierajho/Home/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.financierajho.Home
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.financierajho.R
+import com.example.financierajho.databinding.ActivityHomeBinding
+import com.example.financierajho.databinding.ActivityLoginBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/example/financierajho/Home/HomeFragment.kt
+++ app/src/main/java/com/example/financierajho/Home/HomeFragment.kt
@@ -1,22 +1,24 @@
-package com.example.financierajho
+package com.example.financierajho.Home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.financierajho.databinding.FragmentHomeBinding
 import com.example.financierajho.databinding.FragmentLoginDocumentBinding
 
-class LoginDocumentFragment : Fragment() {
 
-    var _binding: FragmentLoginDocumentBinding? = null
+class homeFragment : Fragment() {
+
+    var _binding: FragmentHomeBinding? = null
     val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginDocumentBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
