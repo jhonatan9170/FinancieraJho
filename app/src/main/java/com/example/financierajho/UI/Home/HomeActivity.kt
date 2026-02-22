bbf19193e5c2dcfa8d87a1c9a86503e9658dcdf1
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2026-02-22 13:42:15 -0500

    appBar

--- app/src/main/java/com/example/financierajho/UI/Home/HomeActivity.kt
+++ app/src/main/java/com/example/financierajho/UI/Home/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.example.financierajho.UI.Home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.financierajho.R
 import com.example.financierajho.databinding.ActivityHomeBinding
@@ -12,6 +14,7 @@ import com.example.financierajho.databinding.FragmentDocumentLoginBinding
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -21,9 +24,17 @@ class HomeActivity : AppCompatActivity() {
 
     private fun setupBottomNavView(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
+        setSupportActionBar(binding.toolbar)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.productsFragment,
+                R.id.benefitsFragment,
+                R.id.myAccountFragment
+            )
+        )
+        binding.toolbar.setupWithNavController(navController,appBarConfiguration)
         binding.bottonNavView.setupWithNavController(navController)
-
     }
 
 }
