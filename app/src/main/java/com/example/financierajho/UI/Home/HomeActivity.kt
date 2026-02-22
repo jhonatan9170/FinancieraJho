9a6be8ae74e0062c18eeeb0b65c3476743c32777
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2026-02-22 13:01:11 -0500

    bottomnavigationview

--- app/src/main/java/com/example/financierajho/UI/Home/HomeActivity.kt
+++ app/src/main/java/com/example/financierajho/UI/Home/HomeActivity.kt
@@ -2,6 +2,10 @@ package com.example.financierajho.UI.Home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.financierajho.R
 import com.example.financierajho.databinding.ActivityHomeBinding
 import com.example.financierajho.databinding.FragmentDocumentLoginBinding
 
@@ -12,5 +16,14 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupBottomNavView()
     }
+
+    private fun setupBottomNavView(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottonNavView.setupWithNavController(navController)
+
+    }
+
 }
