ebb5e95f85088f858905c881b0773c829b0c4272
jhonatan9170 <65328388+jhonatan9170@users.noreply.github.com>
2026-02-21 22:52:07 -0500

    add benefits fragment

---
+++ app/src/main/java/com/example/financierajho/UI/Home/Benefits/BenefitsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.financierajho.UI.Home.Benefits
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import com.example.financierajho.R
+import com.example.financierajho.databinding.FragmentBenefitsBinding
+import com.example.financierajho.databinding.FragmentHomeBinding
+
+class BenefitsFragment : Fragment() {
+    var _binding: FragmentBenefitsBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBenefitsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.webview.webViewClient = WebViewClient()
+        binding.webview.settings.javaScriptEnabled = true
+        binding.webview.loadUrl("https://ohbeneficios.tarjetaoh.pe/")
+    }
+
+}
